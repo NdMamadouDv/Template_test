6fe53997607d24b009d8c2e57e4ba21201c643a5
Mamadou Ndiaye <nd.mamadou.dv@gmail.com>
2023-05-31 11:30:15 +0200

    test

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -4,5 +4,10 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1 className="text-red-500">Bienvenue sur le test de template</h1>;
+  return (
+    <div className="">
+      <h1 className="text-red-500">Bienvenue sur le test de template</h1>
+      <h2 className="">TEEEEEEEEEEEEEEEEEEEEEFSDQEQSDTTT</h2>
+    </div>
+  );
 }

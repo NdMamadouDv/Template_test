6ddd24e6fbcdc1ac7eba82e5a6bca08ae9efaaf6
Mamadou Ndiaye <nd.mamadou.dv@gmail.com>
2023-06-01 11:14:25 +0200

    Ajout de la page d'accueil

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,19 +1,31 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Typography from "@/Components/ui/Typography";
+import { Container } from "@/Components/layout/Container";
 
+import { AiOutlineArrowRight } from "react-icons/ai";
+import Layout from "@/Components/layout/Layout";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className=" layout max-w-lg mx-auto min-h-screen">
-      <Typography
-        variant="lead"
-        className="text-3xl text-red-500 "
-        font="emphasize"
-      >
-        Bonjour
-      </Typography>
-    </div>
+    <>
+      <Layout>
+        <Container className="flex items-center justify-center min-h-screen">
+          <div className="flex items-center gap-2">
+            {" "}
+            <Typography>Bonjour</Typography>
+            <Image
+              src="/firebase-1.svg"
+              width={300}
+              height={300}
+              className="w-12 h-auto"
+              alt=""
+            />
+          </div>
+        </Container>
+      </Layout>
+    </>
   );
-}
+};
+export default Home;

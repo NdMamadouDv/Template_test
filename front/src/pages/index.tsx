612f2a095d72b7d749a13ccb832b9b919d4b9c22
Mamadou Ndiaye <nd.mamadou.dv@gmail.com>
2023-06-01 11:48:21 +0200

    ajout de contenu

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Container } from "@/Components/layout/Container";
 
 import { AiOutlineArrowRight } from "react-icons/ai";
 import Layout from "@/Components/layout/Layout";
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
@@ -13,13 +14,28 @@ const Home = () => {
       <Layout>
         <section className="bg-orange-500/80 ">
           <Container className="  min-h-screen">
-            <div className=" h-full grid items-center justify-center py-40">
-              <Typography className="h1">
-                Créons ensemble un site qui vende.
-              </Typography>
-              <Typography className="font-sm">
-                Coder est difficile, et
-              </Typography>
+            <div className=" h-full grid items-center justify-center py-36">
+              <div className="space-y-6">
+                <Typography
+                  variant="display"
+                  component="h1"
+                  className="text-white"
+                >
+                  Créons ensemble un site web qui vende.
+                </Typography>
+                <Typography variant="lead">
+                  Coder est difficile, et vous n'avez pas de temps. Ca tombe
+                  bien, nos équipes de développeur dévoués ont mis en place des
+                  technologies pour créer les sites internet de demain.
+                </Typography>
+
+                <Link href="/editor">
+                  {" "}
+                  <Typography variant="body-base">
+                    Essayer maintenant
+                  </Typography>{" "}
+                </Link>
+              </div>
             </div>
           </Container>
         </section>

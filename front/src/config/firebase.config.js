4477b633d383cdaaee20acf7844d61613f60d362
AlexandreBello <belloalexandre83@gmail.com>
2023-06-01 10:05:22 +0200

    Firebase config and deployement update

---
+++ front/src/config/firebase.config.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAbkkMDWyoXpYlDcMrlsr53yLfY0aA170o",
+	authDomain: "mamadouprodjeccheune.firebaseapp.com",
+	projectId: "mamadouprodjeccheune",
+	storageBucket: "mamadouprodjeccheune.appspot.com",
+	messagingSenderId: "20842674791",
+	appId: "1:20842674791:web:ba7364fd06fb2658a5f2c2",
+};
+
+const app = initializeApp(firebaseConfig);

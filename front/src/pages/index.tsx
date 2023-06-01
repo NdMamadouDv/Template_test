5d2339e1b090fcced76173d4322ab14daef0ae11
Mamadou Ndiaye <nd.mamadou.dv@gmail.com>
2023-06-01 12:00:15 +0200

    Ajout de ocntenu section 1

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -19,23 +19,53 @@ const Home = () => {
                 <Typography
                   variant="display"
                   component="h1"
-                  className="text-white"
+                  className="text-white leading-12"
                 >
-                  Créons ensemble un site web qui vende.
+                  Créons ensemble un site web qui vend.
                 </Typography>
-                <Typography variant="lead" className="text-gray-500">
+                <Typography variant="lead" className="text-gray-500/60">
                   Coder est difficile, et vous n'avez pas de temps. Ca tombe
                   bien, nos équipes de développeur dévoués ont mis en place des
                   technologies pour créer les sites internet de demain.
                 </Typography>
 
-                <Link href="/editor">
+                <Link href="/editor" className="">
                   {" "}
-                  <Typography variant="body-base">
+                  <Typography variant="body-base" className="mt-8">
                     Essayer maintenant
                   </Typography>{" "}
                 </Link>
               </div>
+            </div>
+          </Container>
+        </section>
+        <section className="my-8">
+          <Container className="grid xl:grid-cols-3 items-center justify-center">
+            <div className="">
+              <Image
+                src="/equipe-sur-un-projet.jpg"
+                height={300}
+                width={300}
+                alt=""
+              />
+              <Typography className="" variant="body-sm" weight="font-normal">
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                cupiditate eos, iste obcaecati amet ut laudantium nostrum
+                aliquam, asperiores reprehenderit non at autem provident itaque,
+                quisquam consectetur qui temporibus repellendus corrupti quos
+                beatae tempore suscipit? Fuga, unde autem. Cupiditate dolor
+                maxime animi temporibus sint cumque quod nesciunt inventore
+                explicabo neque.
+              </Typography>
+            </div>
+            <div className="">
+              <Image src="" height={300} width={300} />
+              <Typography className=""></Typography>
+            </div>
+            <div className="">
+              <Image src="" height={300} width={300} />
+              <Typography className=""></Typography>
             </div>
           </Container>
         </section>

a10cc2d1556aa22fab390f181d4603d44923ae05
Mamadou Ndiaye <nd.mamadou.dv@gmail.com>
2023-06-01 14:02:27 +0200

    paddings & stuff

--- front/src/modules/home/components/Technologies.tsx
+++ front/src/modules/home/components/Technologies.tsx
@@ -3,54 +3,51 @@ import Typography from "@/Components/ui/Typography";
 import Image from "next/image";
 
 const Technologies = () => {
-	return (
-		<section className="my-8 ">
-			<Container className="grid xl:grid-cols-3 items-center justify-center gap-10 border-t">
-				<Typography variant="h2">Notre technologie</Typography>
-				<div className="flex gap-4 items-center justify-center">
-					<Image
-						src="/reunion-equipe.jpg"
-						height={550}
-						width={550}
-						alt=""
-						className="rounded-tr-[5rem]
+  return (
+    <section className="my-8 ">
+      <Container className="grid xl:grid-cols-3 items-center justify-center gap-10 border-t">
+        <Typography variant="h2">Notre technologie</Typography>
+        <div className="xl:flex gap-6 items-center justify-center space-y-6">
+          <Image
+            src="/reunion-equipe.jpg"
+            height={550}
+            width={550}
+            alt=""
+            className="rounded-tr-[5rem]
         rounded-bl-[5rem]"
-					/>
-					<Typography
-						className="text-gray-500/90"
-						variant="body-sm"
-						weight="regular">
-						{" "}
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-						cupiditate eos, iste obcaecati amet ut laudantium nostrum aliquam,
-						asperiores reprehenderit non at autem provident itaque, quisquam
-						consectetur qui temporibus repellendus corrupti quos beatae tempore
-						suscipit? Fuga, unde autem. Cupiditate dolor maxime animi temporibus
-						sint cumque quod nesciunt inventore explicabo neque.
-					</Typography>
-				</div>
-				<div className="flex gap-4 items-center justify-center border-t py-8">
-					<Image
-						src="/femme_tableau.jpg"
-						height={300}
-						width={300}
-						alt=""
-					/>
-					<Typography
-						className="text-gray-500/90"
-						variant="body-sm"
-						weight="regular">
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
-						perspiciatis, quo veniam distinctio sit iure possimus mollitia
-						voluptatibus numquam aspernatur aperiam delectus saepe? Omnis itaque
-						nulla, unde sed quas quibusdam, et nobis consequuntur consectetur
-						non iste error natus, maiores asperiores aspernatur repudiandae
-						voluptatum. Repellendus, obcaecati.
-					</Typography>
-				</div>
-			</Container>
-		</section>
-	);
+          />
+          <Typography
+            className="text-gray-500/90"
+            variant="body-sm"
+            weight="regular"
+          >
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+            cupiditate eos, iste obcaecati amet ut laudantium nostrum aliquam,
+            asperiores reprehenderit non at autem provident itaque, quisquam
+            consectetur qui temporibus repellendus corrupti quos beatae tempore
+            suscipit? Fuga, unde autem. Cupiditate dolor maxime animi temporibus
+            sint cumque quod nesciunt inventore explicabo neque.
+          </Typography>
+        </div>
+        <div className="flex gap-4 items-center justify-center border-t py-8">
+          <Image src="/femme_tableau.jpg" height={300} width={300} alt="" />
+          <Typography
+            className="text-gray-500/90"
+            variant="body-sm"
+            weight="regular"
+          >
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
+            perspiciatis, quo veniam distinctio sit iure possimus mollitia
+            voluptatibus numquam aspernatur aperiam delectus saepe? Omnis itaque
+            nulla, unde sed quas quibusdam, et nobis consequuntur consectetur
+            non iste error natus, maiores asperiores aspernatur repudiandae
+            voluptatum. Repellendus, obcaecati.
+          </Typography>
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default Technologies;

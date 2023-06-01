cfaf4771a28f9f825f55d61877034fb9d5f9242a
Mamadou Ndiaye <nd.mamadou.dv@gmail.com>
2023-06-01 12:16:15 +0200

    1 hero 2 sections

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -39,6 +39,116 @@ const Home = () => {
             </div>
           </Container>
         </section>
+        <section className="my-8">
+          <Container className="grid xl:grid-cols-3 items-center justify-center gap-10 ">
+            <Typography variant="h2">Notre savoir faire</Typography>
+            <div className="flex gap-4 items-center justify-center">
+              <Image
+                src="/equipe-sur-un-projet.jpg"
+                height={300}
+                width={300}
+                alt=""
+              />
+              <Typography
+                className="text-gray-500/90"
+                variant="body-sm"
+                weight="regular"
+              >
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                cupiditate eos, iste obcaecati amet ut laudantium nostrum
+                aliquam, asperiores reprehenderit non at autem provident itaque,
+                quisquam consectetur qui temporibus repellendus corrupti quos
+                beatae tempore suscipit? Fuga, unde autem. Cupiditate dolor
+                maxime animi temporibus sint cumque quod nesciunt inventore
+                explicabo neque.
+              </Typography>
+            </div>
+            <div className="flex gap-4 items-center justify-center">
+              <Image src="/femme_tableau.jpg" height={300} width={300} alt="" />
+              <Typography
+                className="text-gray-500/90"
+                variant="body-sm"
+                weight="regular"
+              >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Voluptate perspiciatis, quo veniam distinctio sit iure possimus
+                mollitia voluptatibus numquam aspernatur aperiam delectus saepe?
+                Omnis itaque nulla, unde sed quas quibusdam, et nobis
+                consequuntur consectetur non iste error natus, maiores
+                asperiores aspernatur repudiandae voluptatum. Repellendus,
+                obcaecati.
+              </Typography>
+            </div>
+            <div className="flex gap-4 items-center justify-center">
+              <Image
+                src="/sitemap-design.jpg"
+                height={300}
+                width={300}
+                alt=""
+              />
+              <Typography
+                className="text-gray-500/90"
+                variant="body-sm"
+                weight="regular"
+              >
+                {" "}
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla,
+                est. Quibusdam nulla culpa pariatur nobis vitae hic sit
+                necessitatibus, provident perspiciatis? Quis amet eius saepe
+                deserunt quidem vel beatae sunt nostrum, qui excepturi officia
+                incidunt delectus autem aspernatur tempore odit? Beatae, rem
+                totam! Nostrum!
+              </Typography>
+            </div>
+          </Container>
+        </section>
+
+        <section className="my-8 ">
+          <Container className="grid xl:grid-cols-3 items-center justify-center gap-10 border-t">
+            <Typography variant="h2">Notre technologie</Typography>
+            <div className="flex gap-4 items-center justify-center">
+              <Image
+                src="/reunion-equipe.jpg"
+                height={550}
+                width={550}
+                alt=""
+                className="rounded-tr-[5rem]
+                rounded-bl-[5rem]"
+              />
+              <Typography
+                className="text-gray-500/90"
+                variant="body-sm"
+                weight="regular"
+              >
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                cupiditate eos, iste obcaecati amet ut laudantium nostrum
+                aliquam, asperiores reprehenderit non at autem provident itaque,
+                quisquam consectetur qui temporibus repellendus corrupti quos
+                beatae tempore suscipit? Fuga, unde autem. Cupiditate dolor
+                maxime animi temporibus sint cumque quod nesciunt inventore
+                explicabo neque.
+              </Typography>
+            </div>
+            <div className="flex gap-4 items-center justify-center border-t py-8">
+              <Image src="/femme_tableau.jpg" height={300} width={300} alt="" />
+              <Typography
+                className="text-gray-500/90"
+                variant="body-sm"
+                weight="regular"
+              >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Voluptate perspiciatis, quo veniam distinctio sit iure possimus
+                mollitia voluptatibus numquam aspernatur aperiam delectus saepe?
+                Omnis itaque nulla, unde sed quas quibusdam, et nobis
+                consequuntur consectetur non iste error natus, maiores
+                asperiores aspernatur repudiandae voluptatum. Repellendus,
+                obcaecati.
+              </Typography>
+            </div>
+          </Container>
+        </section>
       </Layout>
     </>
   );

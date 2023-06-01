5da732a7464fc987ac3bc9d3c74e80d17019fe42
Mamadou Ndiaye <nd.mamadou.dv@gmail.com>
2023-06-01 10:43:06 +0200

    Merci le peuple

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Typography from "@/Components/ui/Typography";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,13 @@ export default function Home() {
     <div className="">
       <h1 className="text-red-500">Bienvenue sur le test de template</h1>
       <h2 className="">TEEEEEEEEEEEEEEEEEEEEEFSDQEQSDTTT</h2>
+      <Typography
+        variant="lead"
+        className="text-3xl text-red-500 "
+        font="emphasize"
+      >
+        Bonjour
+      </Typography>
     </div>
   );
 }
